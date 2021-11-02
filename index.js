e1beff84404f166c170a608210665b8fbc2cec43
rahul shah <rkofficial568@gmail.com>
2021-11-02 21:11:16 +0530

    t

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const measureKelvin = function () {
-  const measurement = {
-    type: 'temp',
-    unit: 'celsius',
-    value: Number(prompt('degrees celsius:')),
-  };
-  console.table(measurement);
-
-  const kelvin = measurement.value + 273;
-  return kelvin;
-};
-console.log(measureKelvin());
+'use strict';
