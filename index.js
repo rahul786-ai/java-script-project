3cb4dfe7fd985564987127a623e698ad5ee337ad
rahul shah <rkofficial568@gmail.com>
2021-11-17 01:06:16 +0530

    init

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 
-console.log(document.querySelector('.message'));
+console.log(
+  (document.querySelector('.message').textContent = 'Correct Number 🎉')
+);
+document.querySelector('.number').textContent = 23;
+document.querySelector('.score').textContent = 15;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
